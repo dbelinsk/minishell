0424134aa0ce326a1f6a851409e5f5482eeaf49b
Dainis Belinskis <dbelinsk@gmail.com>
2020-10-02 17:19:44 +0200

    ft_isspace added to library

--- lib/libft/libft.h
+++ lib/libft/libft.h
@@ -28,6 +28,7 @@ int					ft_isdigit(int c);
 int					ft_isalnum(int c);
 int					ft_isascii(int c);
 int					ft_isprint(int c);
+int					ft_isspace(char c);
 int					ft_toupper(int c);
 int					ft_tolower(int c);
 char				*ft_strchr(const char *s, int c);
